4e05326a006dfa086f07e2eb487515f04036d197
withcenterdev5 <withcenter.dev5@gmail.com>
2023-12-08 11:07:00 +0800

    version 1

--- example/calcu_example.dart
+++ example/calcu_example.dart
@@ -1,6 +1,1 @@
-import 'package:calcu/calcu.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+void main() {}
--- lib/src/calcu_base.dart
+++ lib/src/calcu_base.dart
@@ -1,6 +1,3 @@
-// TODO: Put public facing types in this file.
+import 'package:function_tree/function_tree.dart';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
-}
+num calculate(String expression) => expression.interpret();
